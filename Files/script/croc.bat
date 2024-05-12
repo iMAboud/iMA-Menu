ff6ef9531fecedcc089e551c96ed7e1cec69227e
iMAboud <80198949+iMAboud@users.noreply.github.com>
2024-05-12 14:50:19 +0300

    Update croc.bat

--- Files/script/croc.bat
+++ Files/script/croc.bat
@@ -1,3 +1,16 @@
 @echo off
+chcp 65001 > nul
 
+
+
+echo                                   ▄█     ▄▄▄▄███▄▄▄▄      ▄████████ 
+echo                                  ███   ▄██▀▀▀███▀▀▀██▄   ███    ███ 
+echo                                  ███▌  ███   ███   ███   ███    ███ 
+echo                                  ███▌  ███   ███   ███   ███    ███ 
+echo                                  ███▌  ███   ███   ███ ▀███████████ 
+echo                                  ███   ███   ███   ███   ███    ███ 
+echo                                  ███   ███   ███   ███   ███    ███ 
+echo                                  █▀     ▀█   ███   █▀    ███    █▀  
+echo.                                                 
+echo.
 powershell -NoExit -Command "Croc send --code SET_YOUR_CODE_HERE \"$(Get-Clipboard)\"" 
